118bcd37d4469e6b0d26e8101ce60bc6eeaa0f14
LineageOS Builder <builder@localhost>
2026-08-21 06:04:20 +0000

    remove reboot button from home header

--- includes/header.php
+++ includes/header.php
@@ -17,11 +17,5 @@ $board = $state['board'] ?? ['hostname' => 'orangepizero2', 'uptime' => '10m', '
             <span style="display:inline-block; width:7px; height:7px; border-radius:50%; background:#10b981; box-shadow:0 0 8px #10b981;"></span>
             <span id="headerUptimeText">Aktif: <?= htmlspecialchars($board['uptime'] ?? '10m') ?></span>
         </div>
-
-        <!-- Reboot Trigger Button -->
-        <button type="button" class="btn-new-device" id="btnHeaderReboot" style="color: #ef4444;" title="Mulai Ulang Orange Pi Zero 2">
-            <i class="bi bi-power" style="font-size: 15px;"></i>
-            <span>Mulai Ulang</span>
-        </button>
     </div>
 </header>
